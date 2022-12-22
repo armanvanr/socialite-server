8e4cf884691b5089e98e6e253293bb44a2302476
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2022-12-22 12:31:26 +0700

    welcome to server

--- index.js
+++ index.js
@@ -5,19 +5,25 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Users = require('./api/Users');
 const Posts = require('./api/Posts');
+const path = require('path');
+const public = __dirname + "/public/";
 const app = express();
 
 //Configurations
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({limit: '500kb'}));
+app.use(bodyParser.json({ limit: '500kb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //Routes
 const routes = [Users, Posts];
 app.use('/', routes)
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(public + "index.html"));
+});
 
 //Mongoose Setup
 const PORT = process.env.PORT || 6001;
